260e01d2a3d11c234599385e44fad603442bab5c
nruiz <nruiz@tuenti.com>
2013-12-16 06:38:02 -0800

    Renaming all classes

--- TaskExecutor/test/com/async/WorkerInteractorInterface.java
+++
@@ -1,9 +0,0 @@
-package com.async;
-
-import java.util.concurrent.CountDownLatch;
-
-public interface WorkerInteractorInterface {
-
-	public abstract void execute(int workerId, AchoTaskCallback callback, CountDownLatch doneSignal);
-
-}
